890e229510a60b699739c3cbf3aa2d90b1aefbf7
pkalandhar-Basha <pkalandhar5@gmail.com>
2026-04-19 19:44:06 +0530

    lab8b

---
+++ src/main/java/com/CookieServlet/CookieServlet.java
@@ -0,0 +1,74 @@
+package com.CookieServlet;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet("/cookieDemo")
+public class CookieServlet extends HttpServlet {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        String name = request.getParameter("username");
+        int visitCount = 1;
+
+        // Read existing cookies
+        Cookie[] cookies = request.getCookies();
+
+        if (cookies != null) {
+            for (Cookie c : cookies) {
+
+                if (c.getName().equals("visitCount")) {
+                    visitCount = Integer.parseInt(c.getValue()) + 1;
+                }
+
+                if (c.getName().equals("username")) {
+                    name = c.getValue(); // retain old name
+                }
+            }
+        }
+
+        // Create/Update cookies
+        Cookie nameCookie = new Cookie("username", name);
+        Cookie countCookie = new Cookie("visitCount", String.valueOf(visitCount));
+
+        // Set expiry (demo: 30 seconds)
+        nameCookie.setMaxAge(30);
+        countCookie.setMaxAge(30);
+
+        response.addCookie(nameCookie);
+        response.addCookie(countCookie);
+
+        // Output response
+        out.println("<html><body>");
+
+        out.println("<h2>Welcome back " + name + "!</h2>");
+        out.println("<h3>You have visited this page " + visitCount + " times.</h3>");
+
+        out.println("<h3>Cookie List with Values:</h3>");
+
+        if (cookies != null) {
+            for (Cookie c : cookies) {
+                out.println("<p>");
+                out.println("Name: " + c.getName() + "<br>");
+                out.println("Value: " + c.getValue() + "<br>");
+                out.println("</p><hr>");
+            }
+        } else {
+            out.println("<p>No cookies available</p>");
+        }
+
+        out.println("<p>(Cookies expire in 30 seconds)</p>");
+        out.println("</body></html>");
+    }
+}
